020a5c4e6ff4ecb3f358fe4a04868de74a09b091
CharlotteMaschke <55599742+CharlotteMaschke@users.noreply.github.com>
2020-12-02 14:56:46 -0500

    Update stability_measure.py

--- helper_functions/stability_measure.py
+++ helper_functions/stability_measure.py
@@ -17,9 +17,16 @@ def stability (X_temp_LD, X_test_LD,k):
     print("K-MEANS STARTED: k = {}".format(k))
     sys.stdout.flush()  # This is needed when we use multiprocessing
 
-    kmeans = KMeans(n_clusters=k, max_iter=100, n_init=1)
-    kmeans.fit(X_temp_LD)  # fit the classifier on X_template
-    S_temp = kmeans.predict(X_test_LD)
+    print(X_temp_LD)
+    sys.stdout.flush()  # This is needed when we use multiprocessing
+
+    print(X_test_LD)
+    sys.stdout.flush()  # This is needed when we use multiprocessing
+
+    print(k)
+    #kmeans = KMeans(n_clusters=k, max_iter=100, n_init=1)
+    #kmeans.fit(X_temp_LD)  # fit the classifier on X_template
+    #S_temp = kmeans.predict(X_test_LD)
     """
 
     kmeans = KMeans(n_clusters=k, max_iter=1000, n_init=100)
