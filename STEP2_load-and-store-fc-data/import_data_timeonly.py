fix aec calculation

STEP2_load-and-store-fc-data/import_data_timeonly.py
@@ -69,7 +69,7 @@ for frequency in FREQUENCY:
 
                 data = scipy.io.loadmat(part_in)
                 if mode == "AEC":
-                    data = data["aec_tofill".format(mode[0])]
+                    data = data["aec_tofill"]
                 else:
                     data = data["{}pli_tofill".format(mode[0])]
 

STEP4_calculate_differentiation/calculate_differentiation.py
@@ -83,7 +83,7 @@ for p_id in IDS:
     # load .mat and extract data
     data = loadmat(data_path)
     if mode == "AEC":
-        data = data["aec_tofill".format(mode[0])]
+        data = data["aec_tofill"]
     else:
         data = data["{}pli_tofill".format(mode[0])]
     channel = scipy.io.loadmat(channels_path)['channels'][0][0]